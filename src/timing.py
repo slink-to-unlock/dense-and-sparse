dfdcc7a3676d71b19390ab28596ae690ca12463a
subin <subinaksl@naver.com>
2023-10-09 21:16:39 +0900

    add video player function

--- src/timing.py
+++ src/timing.py
@@ -1,3 +1,4 @@
+import cv2
 class Timing:
     def __init__(self, min_: int, sec_: int) -> None:
         assert min_ >= 0 and type(min_) is int
@@ -16,6 +17,44 @@ class Timing:
     def to_stamp(self):
         return f'00:{self.min_:02}:{self.sec_:02}'
 
+
+def videoplayer(video_path):
+    capture = cv2.VideoCapture(video_path)
+    times = []
+    while capture.isOpened():
+        run, frame = capture.read()
+        key = cv2.waitKeyEx(30)
+        # print(capture.get(cv2.CAP_PROP_POS_MSEC))
+        if not run:
+            print("[프레임 수신 불가] - 종료합니다")
+            break
+        img = cv2.cvtColor(frame, cv2.IMREAD_COLOR)
+        img = cv2.resize(img, (520, 520))
+
+        cv2.imshow('video', img)
+        # cv2.waitKey(30)
+        if key == ord('s'):
+            cv2.waitKeyEx()
+        if key == ord('p'):
+            cv2.waitKeyEx(30)
+        if key == ord('q'):
+            # 프로그램 종료
+            break
+        if key == ord('t'):
+            t = capture.get(cv2.CAP_PROP_POS_MSEC)
+            times.append(int(t))
+
+        if key == 0x250000:
+            current_time = capture.get(cv2.CAP_PROP_POS_MSEC)
+            capture.set(cv2.CAP_PROP_POS_MSEC, current_time - 1000)
+        if key == 0x270000:
+            current_time = capture.get(cv2.CAP_PROP_POS_MSEC)
+            capture.set(cv2.CAP_PROP_POS_MSEC, current_time + 1000)
+
+    capture.release()
+    cv2.destroyAllWindows()
+    # print('times:',times)
+    return times
 
 def to_dict(start: Timing, end: Timing) -> dict:
     return {
