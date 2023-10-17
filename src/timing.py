feee4ea98e62c012a2e734f8096be25cd9c8c675
ProtossDragoon <dlwkdgn1@naver.com>
2023-10-17 15:54:35 +0900

    refactor: sec와 misec을 조작했을 때 다른 하나가 자동적으로 조정되도록 변경

--- src/timing.py
+++ src/timing.py
@@ -10,25 +10,46 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 class Timing:
-    def __init__(self, misec_: int) -> None:
-        assert type(misec_) is int, f'type(misec_) is {type(misec_)}, not int'
-        assert misec_ >= 0, f'misec_ is {misec_}, not >= 0'
-        self.misec_ = misec_
-        self.totalsec_ = round(self.misec_/1000)
+    def __init__(self, misec: int) -> None:
+        assert type(misec) is int, f'type(misec) is {type(misec)}, not int'
+        assert misec >= 0, f'misec is {misec}, not >= 0'
+        self._misec = None
+        self._sec = None
+        self.misec = misec # call setter
 
     def __str__(self) -> str:
         return self.to_stamp()
 
+    @property
+    def misec(self) -> int:
+        return self._misec
+
+    @misec.setter
+    def misec(self, v):
+        self._misec = v
+        self._sec = round(self.misec / 1000)
+
+    @property
+    def sec(self) -> float:
+        return self._sec
+
+    @sec.setter
+    def sec(self, v):
+        self._sec = v
+        self._misec = 1000 * v
+
     def to_stamp(self):
         h = 0
-        m = self.totalsec_ // 60
+        m = self.sec // 60
         if m >= 60:
             h = m // 60
             m = m % 60
-        s = self.totalsec_ % 60
+        s = self.sec % 60
         return f'{h:02}:{m:02}:{s:02}'
 
-    def until(self, end) -> dict:
+    def until(self, end, pad: bool = False) -> dict:
+        if pad:
+            raise NotImplementedError()
         return {
             'start_time': self.to_stamp(),
             'end_time': end.to_stamp(),
