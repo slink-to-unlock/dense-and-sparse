9738a0b069d303acc74b7e52fe9f980f697fa7bd
ProtossDragoon <dlwkdgn1@naver.com>
2023-11-11 18:23:33 +0900

    fix: UI 레이블링 시 단계(step)가 수정되지 않는 문제 수정

--- src/app.py
+++ src/app.py
@@ -4,8 +4,7 @@ import time
 
 import streamlit as st
 
-
-clips_directory = os.path.join('test-ws', 'sparse-label')
+clips_directory = os.path.join('volume', 'test-ws', 'sparse-label')
 label_json = os.path.join(clips_directory, 'result.json')
 
 
@@ -86,7 +85,7 @@ for video in videos:
     if video.get('clip_name') == os.path.basename(selected_video_path):
         video['sparse_label'] = [label]
         video['meta_info'] = {
-            "step": "애벌",
+            "step": 단계,
             "scrubber_holding_hand": 수세미를_든_손 == '보임',
             "dishes_holding_hand": 식기를_든_손 == '보임',
             "dishes_in_roi": 지정한_위치_내에_식기 == '예',
