88bdec0f80b46a14f8f2a098803d851626ddeb4f
ProtossDragoon <dlwkdgn1@naver.com>
2024-06-15 14:20:18 +0900

    Feat: 모듈을 통한 streamlit 실행 시 `port` 를 입력받을 수 있도록 수정

--- sparse_to_dense/cli/web.py
+++ sparse_to_dense/cli/web.py
@@ -1,8 +1,24 @@
+# 내장
+import argparse
+
 # 서드파티
 from streamlit.web import cli
 
 # 프로젝트
 from sparse_to_dense import streamlit_home_py_path
 
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Run Streamlit app')
+    parser.add_argument(
+        '--port',
+        type=int,
+        default=8501,
+        help='Port to run the Streamlit app on',
+    )
+    return parser.parse_args()
+
+
 if __name__ == '__main__':
-    cli.main_run([streamlit_home_py_path])
+    args = parse_arguments()
+    cli.main_run([streamlit_home_py_path, f'--server.port={args.port}'])
